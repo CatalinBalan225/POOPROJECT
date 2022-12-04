384f0e59988b1798443557ede1e70cf8198b4ddd
CatalinBalan552 <balancatalin512@gmail.com>
2022-12-04 12:28:46 +0200

    Added tickets class

--- main.cpp
+++ main.cpp
@@ -51,13 +51,88 @@ public:
 };
 
 
+class ticket {
+public:
+	int ticketChoice;
+	float noTickets;
+	float ticketCost;
+	static float lastticketCost;
 
+	void ticketDetails() {
+		cout << "These are the types of tickets you can choose for." << endl;
+
+		cout << "1. Buy a standard ticket" << endl;
+		cout << "2.Buy a VIP ticket" << endl;
+
+		cout << "\n To calculate the cost of your ticket" << endl;
+		cout << "Enter the type of ticket you prefer.";
+		cin >> ticketChoice;
+		cout << "Enter the number of tickets you want to buy. ";
+		cin >> noTickets;
+
+		int chooseTicket;
+		if (ticketChoice == 1) {
+			ticketCost = noTickets * 200;
+			cout << "\nYour ticket cost" << ticketCost << " $ for standard ticket" << endl;
+			cout << "Press 1 to confirm or ";
+			cout << "Press 2 to select another type of ticket";
+			cin >> chooseTicket;
+			system("CLS");
+			if (chooseTicket == 1) {
+				lastticketCost = ticketCost;
+				cout << "\nYou have succesfully purchased the standard ticket." << endl;
+				cout << "Go to the menu and take the receipt" << endl;
+			}
+			else if (chooseTicket == 2) {
+				ticketDetails();
+			}
+			else {
+				cout << "Invalid Input! Redirecting to previous menu\nPlease Wait!" << endl;
+				Sleep(999);
+				system("CLS");
+				ticketDetails();
+			}
+		}
+		else if (ticketChoice == 2) {
+			ticketCost = noTickets * 500;
+			cout << "\nYour ticket cost is " << ticketCost << "$ for VIP ticket" << endl;
+
+			cout << "Press 1 to confirm or ";
+			cout << "Press 2 to select another cab";
+			cin >> chooseTicket;
+			system("CLS");
+			if (chooseTicket == 1) {
+				lastticketCost = ticketCost;
+				cout << "\nYou have succesfully purchased the VIP ticket." << endl;
+				cout << "Go to menu and take the receipt" << endl;
+			}
+			else if (chooseTicket == 2) {
+				ticketDetails();
+			}
+			else {
+				cout << "Invalid Input! Redirecting to previous menu\nPlease Wait!" << endl;
+				Sleep(999);
+				system("CLS");
+
+			}
+			cout << "\n Press 1 to redirect to main menu";
+			cin >> chooseTicket;
+			system("CLS");
+			if (chooseTicket == 1) {
+
+			}
+			else {
+
+			}
+		}
+	}
+};
 
 
 
 
 int main() {
-	cout << "Hello World!" << endl;
+	
 
 	return 0;
 }
